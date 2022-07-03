b47410e7caac607dfa7052c442e4ebaa95fc5f00
ivancheg8 <pavlevan@yandex.ru>
2022-07-03 21:21:44 +0300

    work done

--- lesson4_c#.cs
+++ lesson4_c#.cs
@@ -6,18 +6,19 @@
 9012 -> 12
 */
 
-int summa(int a)
+double summa(double a)
 {
-    var b = Convert.ToString(a);
-    int e = 0;
+    string b = Convert.ToString(a);
+    double e = 0;
     for (int i = 0; i < b.Length; i++)
     {
-        e = e + Convert.ToInt32(b[i].ToString());
+        e = e + Convert.ToDouble(b[i].ToString()); //конвертация char в string, а потом в double, чтобы бралось число, а не код числа
+        //e = e + (Char.GetNumericValue(b, i)); //второй вариант, даёт сразу число
     }
     return (e);
 }
 Console.WriteLine("Введи число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+double num = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Сумма чисел: " + summa(num));
 
 
@@ -40,7 +41,7 @@ void massiv()
     for (int i = 0; i < a; i++) nums[i] = rand.Next(1, 100); //заполнение массива рандомными числами 1-99
 
     Console.Write("Массив длинной " + a + ": ");
-    for (int j = 0; j < nums.Length; j++)
+    for (int j = 0; j < nums.Length; j++) //вывод по очереди в консоль
     {
         if (j == nums.Length - 1) Console.Write(nums[j] + "."); //чтобы последний элемент завершался точкой. для красоты
         else Console.Write(nums[j] + ", ");
